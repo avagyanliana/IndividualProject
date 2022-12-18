9c9b8f1a8b7e0dee95bcda3771ceb4c3951bf972
avagyanliana <65074799+avagyanliana@users.noreply.github.com>
2022-12-18 20:27:46 +0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="clv",
+    version="0.1.2",
+    author="Liana Avagyan",
+    author_email="liana_avagyan@edu.aua.am",
+    description="Marketing CLV package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/avagyanliana/IndividualProject.git",
+    packages=setuptools.find_packages(),
+    package_data={'package': ['Online_Retail.xlsx']},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires='>=3.6',
+)
+
+
+# In[ ]:
+
+
+
+
