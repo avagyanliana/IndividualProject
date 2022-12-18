0a7ec9749229eee6ff43d10e294a71f4c5f37444
avagyanliana <65074799+avagyanliana@users.noreply.github.com>
2022-12-18 20:49:26 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="clv",
+    name="IndividualProject",
     version="0.1.2",
     author="Liana Avagyan",
     author_email="liana_avagyan@edu.aua.am",
